9ac3c1ec4346f28f1670e3288297797f2d63e89c
Huzaifa Ali <huzaifali878@gmail.com>
2026-07-16 18:58:22 +0100

    Connected to MongoDB + implemented bcrypt.

--- api/index.js
+++ api/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://Huzaifa:Let-Down-1989-2004@cluster0.7iuysx3.mongodb.net/?appName=Cluster0');
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({request:{username,password}});
-})
+    try {
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt)
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
+});
 
 app.listen(4000);
+//mongodb+srv://Huzaifa:Let-Down-1989-2004@cluster0.7iuysx3.mongodb.net/
+
+//SRV Connection String
+//mongodb+srv://Huzaifa:Let-Down-1989-2004@cluster0.7iuysx3.mongodb.net/?appName=Cluster0
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -9,11 +9,18 @@ export default function RegisterPage() {
 
     async function register(ev) {
         ev.preventDefault();
-        fetch('http://localhost:4000/register', {
-            method: 'POST',
-            body: JSON.stringify({username,password}),
-            headers: {'Content-Type':'application/json'},
-        })
+        const response = await fetch('http://localhost:4000/register', {
+                method: 'POST',
+                body: JSON.stringify({username,password}),
+                headers: {'Content-Type':'application/json'},
+        });
+        console.log(response);
+        if (response.status === 200) {
+            alert('registration successful');
+        } else {
+            alert('registration failed');
+        }
+        
     }
 
     return (
